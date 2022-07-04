e950fc7817893f57d46fd38aed579f90de011fa6
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2022-07-04 19:46:15 -0300

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Venta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+        public int IdCliente { get; set; }
+        public int TotalProducto { get; set; }
+        public decimal MontoTotal { get; set; }
+        public string Contacto { get; set; }
+        public string IdDistrito { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string FechaTexto { get; set; }
+        public string IdTransaccion { get; set; }
+        public List<DetalleVenta> oDetalleVenta { get; set; }
+    }
+}
